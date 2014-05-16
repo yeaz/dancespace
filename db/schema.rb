305895e5162ee33487c94fd9b31a22807f59b44a
Changwhan Yea <yeaz90@gmail.com>
2014-05-16 10:23:11 -0700

    implemented Events CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513035452) do
+ActiveRecord::Schema.define(version: 20140514002144) do
 
   create_table "experiencelinks", force: true do |t|
     t.integer  "collab_id"
