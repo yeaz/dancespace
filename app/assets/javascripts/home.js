6585f4e3051d1c5dad73bdc001ad99e293d92b08
Jake Ayala <jakeayala@gmail.com>
2014-05-27 02:47:30 -0700

    changed events formatting, added animation, reworked unsigned in homepage slightly, changed javascript ordering so navbar collapse works, changed css for footer

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,33 +1,17 @@
 function loadEventsFeed() {
+    $( "#listpane" ).hide(); 
 	url = "eventsfeed";
 	jQuery.get(url, function(data) {
 		document.getElementById("listpane").innerHTML = data;
 	});
+    $( "#listpane" ).fadeIn(500);
 }
 
 function loadStudiosNearby() {
+    $( "#listpane" ).hide();
 	url = "studiosnearby"
 	jQuery.get(url, function(data) {
 		document.getElementById("listpane").innerHTML = data;
 	});
+	$( "#listpane" ).fadeIn(500);
 }
-
-	// $("#evbutton").click(function() {
-	//    $( "#listpane" ).fadeOut( "fast", function() {
-	//     // Animation complete
-	//   });
-
-	// 	  $( "#listpane" ).fadeIn( "slow", function() {
-	//     // Animation complete
-	//     alert("hey fading in");
-	//   });
-	// });
-
-
-	// $("#stbutton").click(function() {
-	// 	  $( "#listpane" ).fadeIn( "slow", function() {
-	//     // Animation complete
-	//     	    alert("hey fading in");
-	//   });
-	// });
-
