44c88fd254f0ed039d76ec2e5bbacea4f561a688
Jake Ayala <jakeayala@gmail.com>
2014-05-27 19:07:03 -0700

    more js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -8,6 +8,7 @@ function loadEventsFeed() {
 	});
 	$( "#finderheader").html("Studio Finder");
 	$( "#eventheader").html("<strong>Events Feed</strong>");
+	$( "#randomheader").html("Randomizer");
     $( "#listpane" ).fadeIn(750);
 	$( "#foot" ).fadeIn(750);
 	// $( "#sidebar" ).fadeIn(750);
@@ -23,23 +24,40 @@ function loadStudiosNearby() {
 	});
 	$( "#finderheader").html("<strong>Studio Finder</strong>");
 	$( "#eventheader").html("Events Feed");
+	$( "#randomheader").html("Randomizer");
 	$( "#listpane" ).fadeIn(750);
 	$( "#foot" ).fadeIn(750);
 	// $( "#sidebar" ).fadeIn(750);
 }
 
-$(document).ready(function() {
-	$( "#jstest" ).click(function() {
-	  $( "#sidebar" ).hide();
-	  $( "#sidebar").html("<h1>Randomizer</h1><p>Find a random dancer or video!</p><p><b>INSERT CONTROLS HERE</b></p>");
-	  $( "#sidebar" ).fadeIn(1000);
+function loadRandomizer() {
+	  $( "#finderheader").html("Studio Finder");
+	  $( "#eventheader").html("Events Feed");
+	  $( "#randomheader").html("<strong>Randomizer</strong>");
 
 	  $( "#listpane" ).hide();
-	  $( "#listpane").html("<h1>Dancer Info</h1><h2>This is where the dancer would go</h2><p style='font-size:35px;'>and info about the dancer...basically use the script to get the data for a dancer and put it in the pane</p>");
-	  $( "#listpane" ).fadeIn(1000);
-
 	  $( "#foot" ).hide();
-	  $( "#foot" ).fadeIn(1000);
-	});
+	  $( "#listpane").html("<h1>Dancer Info</h1><h2>This is where the dancer would go</h2><p style='font-size:35px;font-family:monospace;font-weight:100'>and info about the dancer...basically use the script to get the data for a dancer and put it in the pane...<br><br>where should i put the option for dancer or video?</p>");
+	  $( "#listpane" ).fadeIn(750);
+	  $( "#foot" ).fadeIn(750);
+}
 
-});
+// $(document).ready(function() {
+// 	$( "#rabutton" ).on('click', function() {
+// 	  // $( "#sidebar" ).hide();
+// 	  // $( "#sidebar").html("<h1>Randomizer</h1><p>Find a random dancer or video!</p><p><b>INSERT CONTROLS HERE</b></p>");
+// 	  // $( "#sidebar" ).fadeIn(1000);
+// 	  $( "#finderheader").html("Studio Finder");
+// 	  $( "#eventheader").html("Events Feed");
+// 	  $( "#randomheader").html("<strong>Randomizer</strong>");
+
+
+// 	  $( "#listpane" ).hide();
+// 	  $( "#listpane").html("<h1>Dancer Info</h1><h2>This is where the dancer would go</h2><p style='font-size:35px;'>and info about the dancer...basically use the script to get the data for a dancer and put it in the pane</p>");
+// 	  $( "#listpane" ).fadeIn(1000);
+
+// 	  $( "#foot" ).hide();
+// 	  $( "#foot" ).fadeIn(1000);
+// 	});
+
+// });
