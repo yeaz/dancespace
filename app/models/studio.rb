2603e4197c88022cd9d8e803cb2012b853f954b9
Rupa Shankar <rupsshankar@gmail.com>
2014-05-29 20:21:37 -0700

    Phone validations for studios

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -17,6 +17,27 @@ class Studio < ActiveRecord::Base
   validates :description, presence: { message: 'Please provide a description' }
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
                               message: 'Please provide a valid email address' }
+ validate :check_phone_number, on: :update
+
+  def check_phone_number
+    if phone_area_code == "" and phone_1 == "" and phone_2 == ""
+      return
+    elsif phone_area_code == "" or phone_1 == "" or phone_2 == ""
+      errors.add(:phone, "Incomplete phone number")
+    elsif !numeric?(phone_area_code, 3) or !numeric?(phone_1, 3) or !numeric?(phone_2, 4)
+      errors.add(:phone, "Invalid phone number. Use format: XXX-XXX-XXXX")
+    end
+  end
+
+  def numeric?(num, length)
+    match = /[0-9]+/.match(num)
+    if !match.nil?
+      if match[0].length == length
+        return true
+      end
+    end
+    return false
+  end
 
   def get_address
     return address_line1 + ' ' + address_line2 + ', ' + city + ' ' + state + ' ' + zip_code
