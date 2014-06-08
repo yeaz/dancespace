79ec059319c81fb366210eda6bf167727f108297
Sarah Nader <snader2@stanford.edu>
2014-06-08 14:00:23 -0700

    more UI changes

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -6,8 +6,8 @@ module EventsHelper
   end
 
   def get_event_profile_pic(photo_path)
-    if photo_path.nil? or photo_path == ""
-      return image_tag "studio1.jpg"
+    if !photo_path.nil? or !photo_path == ""
+      return ""
     else
       path = '<img src="/images/' + photo_path + '"/>'
       return path.html_safe
